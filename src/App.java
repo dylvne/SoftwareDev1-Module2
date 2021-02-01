dc8987308b19213a0e52cb03cb39b6d95309deeb
dylvne <delane4.0@gmail.com>
2021-02-01 13:20:44 -0500

    Scanning text files appears to be working

--- src/App.java
+++ src/App.java
@@ -1,9 +1,12 @@
+//import java.io.Console;
 import java.io.File;
+import java.util.Scanner;
 
 public class App {
 
     public static String fileName = "";
     public static void main(String[] args) throws Exception {
+        String text = "";
         System.out.println(new File(".").getAbsoluteFile());
         System.out.println("Please enter the name of the file to scan");
         fileName = System.console().readLine();
@@ -12,6 +15,17 @@ public class App {
 
         if (file.exists()){
             System.out.println("this file exists");
+            Scanner scanner = new Scanner(file);
+            while(scanner.hasNextLine()){
+                text = text + " " + scanner.nextLine();
+            }
+            scanner.close();
+            //System.out.println(text);
+            String[] wordArr = text.split("\\s+|-|—|\\.|\"|\\?|!|;|,|'s|\\r?\\n|:|\\s");
+            for (String string : wordArr) {
+                
+                System.out.println(string );
+            }
         }
         else{
             System.out.println("this file not found");
