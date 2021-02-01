d14902c0a4d66e76ddf760d522ebefb972a040ec
dylvne <delane4.0@gmail.com>
2021-02-01 09:56:02 -0500

    simple check to see if file exists

--- src/App.java
+++ src/App.java
@@ -1,5 +1,19 @@
+import java.io.File;
+
 public class App {
+
+    public static String fileName = "";
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Please enter the name of the file to scan");
+        fileName = System.console().readLine();
+
+        File file = new File(fileName);
+
+        if (file.exists()){
+            System.out.println("this file exists");
+        }
+        else{
+            System.out.println("this file not found");
+        }
     }
 }
